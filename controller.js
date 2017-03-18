6263037c7651f9c89e19748841ac87883475265d
eloimuns <eloimuns@gmail.com>
2017-03-18 03:40:26 +0100

    avennçoos

---
+++ controller.js
@@ -0,0 +1,25 @@
+// Arranquem el Pong
+Game.ready(function() {
+  var pong = Game.start('game', Pong, {});
+});
+
+var tracker;
+window.onload = function() {
+  var video = document.getElementById('video');
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  tracker = new tracking.ColorTracker();
+  tracking.track('#video', tracker, { camera: true });
+  tracker.on('track', function(event) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    event.data.forEach(function(rect) {
+      context.strokeStyle = rect.color;
+      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.font = '11px Helvetica';
+      context.fillStyle = "#fff";
+      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    });
+  });
+  colorController(tracker);
+};
