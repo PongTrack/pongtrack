e0908a0d133ac06c5662891ef737868d9a59a1ee
eloimuns <eloimuns@gmail.com>
2017-03-18 04:35:40 +0100

    puushh colors

--- controller.js
+++ controller.js
@@ -3,35 +3,67 @@ Game.ready(function() {
   var pong = Game.start('game', Pong, {});
 });
 
-var tracker;
+var tracker_p1;
+var tracker_p2;
 window.onload = function() {
-  var video = document.getElementById('video');
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
-  tracker = new tracking.ColorTracker();
-  tracking.track('#video', tracker, { camera: true });
-  tracker.on('track', function(event) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+  // PLAYER 1
+  var video_p1 = document.getElementById('video_p1');
+  var canvas_p1 = document.getElementById('canvas_p1');
+  var context_p1 = canvas_p1.getContext('2d');
+  tracker_p1 = new tracking.ColorTracker();
+  tracking.track('#video_p1', tracker_p1, { camera: true });
+  tracker_p1.on('track', function(event) {
+    context_p1.clearRect(0, 0, canvas_p1.width, canvas_p1.height);
     event.data.forEach(function(rect) {
-      context.strokeStyle = rect.color;
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+      context_p1.strokeStyle = rect.color;
+      context_p1.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context_p1.font = '11px Helvetica';
+      context_p1.fillStyle = "#fff";
+      context_p1.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context_p1.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
     });
   });
-  tracker.setColors([]);
+  tracker_p1.setColors([]);
+
+  // PLAYER 2
+  var video_p2 = document.getElementById('video_p2');
+  var canvas_p2 = document.getElementById('canvas_p2');
+  var context_p2 = canvas_p2.getContext('2d');
+  tracker_p2 = new tracking.ColorTracker();
+  tracking.track('#video_p2', tracker_p2, { camera: true });
+  tracker_p2.on('track', function(event) {
+    context_p2.clearRect(0, 0, canvas_p2.width, canvas_p2.height);
+    event.data.forEach(function(rect) {
+      context_p2.strokeStyle = rect.color;
+      context_p2.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context_p2.font = '11px Helvetica';
+      context_p2.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+      context_p2.fillStyle = "#fff";
+      context_p2.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+    });
+  });
+  tracker_p2.setColors([]);
 };
 
-var colorActual = null;
-function setColor(color){
-  if (colorActual == color){
-    tracker.setColors([]);
-    colorActual = null;
+var colorActual_p1 = null;
+function setColor_p1(color){
+  if (colorActual_p1 == color){
+    tracker_p1.setColors([]);
+    colorActual_p1 = null;
   } else {
-    tracker.setColors([color]);
-    colorActual = color;
+    tracker_p1.setColors([color]);
+    colorActual_p1 = color;
+  }
+}
+
+var colorActual_p2 = null;
+function setColor_p2(color){
+  if (colorActual_p2 == color){
+    tracker_p2.setColors([]);
+    colorActual_p2 = null;
+  } else {
+    tracker_p2.setColors([color]);
+    colorActual_p2 = color;
   }
 }
 
